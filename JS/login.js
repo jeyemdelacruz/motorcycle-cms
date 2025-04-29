8815c92477bf953d9c6e3d3d08d9d72a8d2d05d2
Yumimo <ejoshua.naranjo@gmail.com>
2025-04-30 00:38:38 +0800

    add admin login logout

---
+++ JS/login.js
@@ -0,0 +1,33 @@
+const loginScreen = document.getElementById('login-screen');
+    const dashboard = document.getElementById('dashboard');
+
+    function login() {
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const error = document.getElementById('login-error');
+
+      if (email === "admin@gmail.com" && password === "admin123456") {
+        sessionStorage.setItem("loggedIn", true);
+        showDashboard();
+      } else {
+        error.style.display = "block";
+      }
+    }
+    function logout() {
+        sessionStorage.removeItem("loggedIn");
+        document.getElementById('dashboard').style.display = "none";
+        document.getElementById('login-screen').style.display = "flex";
+      }
+      
+      function showDashboard() {
+        document.getElementById('login-screen').style.display = "none";
+        document.getElementById('dashboard').style.display = "block";
+      }
+
+    // Auto-login if already logged in
+
+    window.onload = () => {
+        if (sessionStorage.getItem("loggedIn")) {
+          showDashboard();
+        }
+    }
